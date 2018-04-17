42d857d186273b77a621526fb088fd58e7d31246
Shun Yao <ysmiracle@hotmail.com>
2018-04-17 03:52:58 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(call_letters:)
+    new_network = Network.new(call_letters: call_letters)
+    self.network = new_network
+  end
 end
