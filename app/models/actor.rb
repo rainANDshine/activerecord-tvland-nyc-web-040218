ab93bf7098033bf68f6127c651f9e309f7750296
Shun Yao <ysmiracle@hotmail.com>
2018-04-17 03:32:52 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,6 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.map {|character| "#{character.name} - #{}"}
+    self.characters.map {|character| "#{character.actor} - #{character.show}"}
   end
 end
